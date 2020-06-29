eb3cb34fbd5edf8317f4687cc2fd08a65979e7a1
Samir Gossain <samir.gossain@gmail.com>
2020-06-29 17:25:13 +0100

    Removes unneeded files and adds dummy header

---
+++ src/components/Header.js
@@ -0,0 +1,22 @@
+import React from "react";
+import Navbar from "react-bootstrap/Navbar";
+import logo from "../logo.svg";
+
+function Header() {
+  return (
+    <Navbar bg="dark" variant="dark">
+      <Navbar.Brand href="#home">
+        <img
+          alt=""
+          src={logo}
+          width="30"
+          height="30"
+          className="d-inline-block align-top"
+        />{" "}
+        React Bootstrap
+      </Navbar.Brand>
+    </Navbar>
+  );
+}
+
+export default Header;
